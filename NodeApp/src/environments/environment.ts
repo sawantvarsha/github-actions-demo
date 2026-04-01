49dc1c91a39da1fd5390223e73d5560f67429849
varsha sawant <87065915+sawantvarsha@users.noreply.github.com>
2026-04-01 15:26:28 +0530

    Delete NodeApp directory

--- NodeApp/src/environments/environment.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: false,
-  assetURL: '../../assets',
-  isSSL: window.location.protocol === 'https:',
-  asseturl:'/assets/',
-  configs: ['GW2_ShowLoginGotoPage', 'MinPasswordLength', 'MaxPasswordLength', 'PasswordHistoryCount'],
-  req_mach_ip: "52.163.118.116",//Added by Evangeline R || 30 Sep 23
-};
--- NodeApp/src/main.ts
+++
@@ -1,6 +0,0 @@
-import { loadManifest } from '@angular-architects/module-federation';
-//Added parameter : true | Anubhav Goyal | Lazy Loading of Remote Entry Files. | 21-Jun-2024
-loadManifest("/assets/mf.manifest.json",true) // Changed by Aditi D on 06-06-24
-.catch(err => console.error(err))
-.then(_ => import('./bootstrap'))
-.catch(err => console.error(err));
