0d5834fdf9c2be0bbc634b7c0edf5aa18676666a
varshasawant <sawant.varsha@finiq.com>
2026-04-01 14:59:27 +0530

    commit node app changes

---
+++ NodeApp/src/environments/environment.dev.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  assetURL: 'assets',
+  // isSSL: window.location.protocol === 'https:',
+  asseturl: './assets/',
+  configs: ['GW2_ShowLoginGotoPage', 'MinPasswordLength', 'MaxPasswordLength', 'PasswordHistoryCount'],
+  req_mach_ip: "52.163.118.116",//Added by Evangeline R || 30 Sep 23
+};
